70eb5f4365ed735fbbcbea515d05ecf4e00621b5
Tikhon Jelvis <tikhon@jelv.is>
2022-01-21 13:42:37 -0800

    Added the fonts I want for my resume

--- flake.nix
+++ flake.nix
@@ -26,22 +26,36 @@
             buildInputs = with pkgs; [ coreutils tex pandoc ];
 
             FONTCONFIG_FILE = pkgs.makeFontsConf {
-              fontDirectories = [
-                self.junction
+              fontDirectories = with packages.fonts; [
+                aller
+                junction
               ];
             };
           };
 
-          junction = pkgs.fetchzip rec {
-            name = "junction-1.0";
-            url = "https://github.com/theleagueof/junction/archive/master.zip";
-            sha256 = "110jsijn3i26d2487c9680w2z8g3bxflxjlsc4yhyc3wbmb7v1b8";
+          fonts = {
+            junction = pkgs.fetchzip rec {
+              name = "junction-1.0";
+              url = "https://github.com/theleagueof/junction/archive/master.zip";
+              sha256 = "110jsijn3i26d2487c9680w2z8g3bxflxjlsc4yhyc3wbmb7v1b8";
 
-            postFetch = ''
-              mkdir -p $out/share/fonts
-              unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
-              unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
-            '';
+              postFetch = ''
+                mkdir -p $out/share/fonts
+                unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
+                unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
+              '';
+            };
+
+            aller = pkgs.fetchzip rec {
+              name = "aller-1.0";
+              url = "https://www.1001fonts.com/download/aller.zip";
+              sha256 = "0fr1s7983iywlvkqw98iyh149ic2paah0yaww9qiapc43vf9zlmf";
+
+              postFetch = ''
+                mkdir -p $out/share/fonts
+                unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
+              '';
+            };
           };
         };
 
